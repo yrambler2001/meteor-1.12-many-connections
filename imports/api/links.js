941b54d17f42dd80d45e5753f7ca19e5e0d72703
Yurii Synyshyn <yrambler2001@gmail.com>
2023-05-29 19:36:06 +0300

    repro 2.12

--- imports/api/links.js
+++ imports/api/links.js
@@ -1,3 +1,11 @@
 import { Mongo } from 'meteor/mongo';
 
 export const LinksCollection = new Mongo.Collection('links');
+LinksCollection.createIndex({ a: 1 })
+LinksCollection.createIndex({ b: 1 })
+LinksCollection.createIndex({ c: 1 })
+LinksCollection.createIndex({ d: 1 })
+LinksCollection.createIndex({ e: 1 })
+LinksCollection.createIndex({ f: 1 })
+LinksCollection.createIndex({ g: 1 })
+LinksCollection.createIndex({ h: 1 })
